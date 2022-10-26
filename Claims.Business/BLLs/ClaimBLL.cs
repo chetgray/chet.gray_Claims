70a4b4687283398877ace6efcb72c9aa9b026b1a
Chet Gray <chet.gray@waystar.com>
2022-10-26 15:55:23 -0400

    ♻ refactor(Business): rename `ClaimsBLL` to `ClaimBLL`

--- Claims.Business/BLLs/ClaimBLL.cs
+++ Claims.Business/BLLs/ClaimBLL.cs
@@ -4,7 +4,7 @@ using Claims.Business.Models.Interfaces;
 
 namespace Claims.Business.BLLs
 {
-    public class ClaimsBLL
+    public class ClaimBLL
     {
         public IClaimModel GetById(int id)
         {
