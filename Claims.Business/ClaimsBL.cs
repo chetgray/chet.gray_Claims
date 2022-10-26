92c2fa4a6e256760d1aaeebeb88b5d8371a16fd5
Chet Gray <chet.gray@waystar.com>
2022-10-26 10:39:20 -0400

    ✨ feat(Business): begin implementing `ClaimsBL`

---
+++ Claims.Business/ClaimsBL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Claims.Models;
+
+namespace Claims.Business
+{
+    public class ClaimsBL
+    {
+        public IClaimModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
