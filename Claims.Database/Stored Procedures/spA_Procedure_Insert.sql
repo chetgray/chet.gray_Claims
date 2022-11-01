3fe8d6f392340be8127933449b9386d7cf107e1a
Chet Gray <chet.gray@waystar.com>
2022-11-01 11:50:21 -0400

    ✨ feat(Database): implement `spA_Procedure_Insert` stored procedure

---
+++ Claims.Database/Stored Procedures/spA_Procedure_Insert.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [dbo].[spA_Procedure_Insert]
+    @code varchar(50)
+    , @name nvarchar(50)
+AS
+
+INSERT
+INTO [dbo].[Procedure] (
+    [Code]
+    , [Name]
+)
+VALUES (
+    @code
+    , @name
+)
+;
+
+SELECT
+    [ProcedureID]
+    , [Code]
+    , [Name]
+FROM
+    [dbo].[Procedure]
+WHERE
+    [ProcedureID] = SCOPE_IDENTITY()
+;
+
+GO
--- Claims.Database/Tables/Procedure.sql
+++ Claims.Database/Tables/Procedure.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Procedure]
 (
     [ProcedureID] INT IDENTITY NOT NULL CONSTRAINT [PK_Procedure_ProcedureID] PRIMARY KEY
-    , [Code] CHAR(10) NOT NULL
+    , [Code] VARCHAR(50) NOT NULL
         CONSTRAINT [AK_Procedure_Code] UNIQUE ([Code])
     , [Name] NVARCHAR(50) NOT NULL
 )
