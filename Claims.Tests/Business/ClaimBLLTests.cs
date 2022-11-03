90608f4880dcefdc9ffe7ce90e1364135b90fccf
Chet Gray <chet.gray@waystar.com>
2022-11-03 17:48:04 -0400

    🚨 test(Business): test that insterting a claim creates a new record
    Claims should *always* create a new record.

--- Claims.Tests/Business/ClaimBLLTests.cs
+++ Claims.Tests/Business/ClaimBLLTests.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Claims.Business.BLLs;
+using Claims.Business.Models;
+using Claims.Business.Models.Interfaces;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Claims.Tests.Business
 {
     [TestClass]
     public class ClaimBLLTests
     {
+        [TestMethod]
+        public void TestInsertEmptyCreatesNewRecord()
+        {
+            // Arrange
+            ClaimBLL claimBLL = new ClaimBLL();
+            IClaimModel claim = new ClaimModel();
+
+            // Act
+            IClaimModel insertedClaim = claimBLL.Insert(claim);
+
+            // Assert
+            Assert.IsNotNull(insertedClaim);
+        }
     }
 }
