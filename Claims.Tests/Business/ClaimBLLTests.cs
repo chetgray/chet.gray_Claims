0b0ff27042ee6ac059a68b27934e914fef005f0a
Chet Gray <chet.gray@waystar.com>
2022-11-04 12:24:40 -0400

    📝 docs(Tests): document `TestInsertEmptyCreatesNewRecord`

--- Claims.Tests/Business/ClaimBLLTests.cs
+++ Claims.Tests/Business/ClaimBLLTests.cs
@@ -9,6 +9,14 @@ namespace Claims.Tests.Business
     [TestClass]
     public class ClaimBLLTests
     {
+        /// <summary>
+        ///     Tests that inserting an empty Claim creates a new record.
+        /// </summary>
+        /// <remarks>
+        ///     All claims are unique, and we can have multiple claims with the
+        ///     same Patient, Carrier, Hospital, and Procedure, so inserting a
+        ///     claim should create a new record no matter what the data is.
+        /// </remarks>
         [TestMethod]
         public void TestInsertEmptyCreatesNewRecord()
         {
